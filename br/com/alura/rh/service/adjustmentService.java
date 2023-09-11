467310ebff175f7d53d1936bc711606ccba63745
HenriqueValente09 <valentedev09@gmail.com>
2023-09-11 08:35:12 -0300

    feat: adding minimum time interval between readjustments

--- br/com/alura/rh/service/adjustmentService.java
+++ br/com/alura/rh/service/adjustmentService.java
@@ -6,6 +6,7 @@ import br.com.alura.rh.model.Employee;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class adjustmentService {
     public void employeeSalaryAdjust(Employee employee, BigDecimal adjustment) {
@@ -14,6 +15,14 @@ public class adjustmentService {
         if (adjustmentPercentage.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("The adjustment can't be higher than 40% of the salary!");
         }
+
+        LocalDate lastAdjustmentDate = employee.getLastAdjustmentDate();
+        LocalDate currentDate = LocalDate.now();
+        long monthsSinceLastAdjustment = ChronoUnit.MONTHS.between(lastAdjustmentDate, currentDate);
+        if (monthsSinceLastAdjustment < 6) {
+            throw new ValidacaoException("The interval between adjustments must be at least 6 months!");
+        }
+
         BigDecimal newSalary = salary.add(adjustment);
         employee.updateSalary(newSalary);
     }
